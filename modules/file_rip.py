991a51df6af72a79f258ce56651b8113ae280259
Josh Vandewillik <joshwillik@gmail.com>
2012-09-28 13:35:17 -0400

    finish error checking, complete v0.1.0 of module

--- modules/file_rip.py
+++ modules/file_rip.py
@@ -1,13 +1,16 @@
 class Ripper(object):
 	def __init__(self, m_file):
-		self.mp3_file=m_file
-		self.main()
+		if mp3(m_file):
+			self.editable = mp3(m_file)
+			self.main()
+			#except ID3NoHeaderError:
+			print "File does not exist, or is not an mp3 file."
 	def main(self):
-		editable   = mp3(self.mp3_file)
-		f_artist   = editable['artist'][0].encode('utf-8')
-		f_title    = editable['title'][0].encode('utf-8')
-		f_cd_num   = editable['discnumber'][0].encode('utf-8')
-		f_song_num = editable['tracknumber'][0].encode('utf-8')
+		#editable   = mp3(self.mp3_file)
+		f_artist   = self.editable['artist'][0].encode('utf-8')
+		f_title    = self.editable['title'][0].encode('utf-8')
+		f_cd_num   = self.editable['discnumber'][0].encode('utf-8')
+		f_song_num = self.editable['tracknumber'][0].encode('utf-8')
 		#print editable
 		if f_cd_num:
 			f_cd_num=f_cd_num[0]
@@ -24,7 +27,11 @@ if __name__ == "__main__":
 	#import parse_config TODO
 	base_args = argv
 	modified = base_args[1]
-	print modified
-	dat = Ripper(modified)
-	print dat
-	print dat.song_data
+	script = base_args[0]
+	try:
+		dat = Ripper(modified)
+	except:
+		print "File does not exist, is corrupted, or is not an mp3"
+	else:
+		print dat
+		print dat.song_data
