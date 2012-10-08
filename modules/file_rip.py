19bc29e81e5f16a9f6132f4054db45da4b64ed24
Josh Vandewillik <joshwillik@gmail.com>
2012-10-08 10:03:33 -0400

    fix file_rip's ripped data names to match values returned by parse_config

--- modules/file_rip.py
+++ modules/file_rip.py
@@ -3,20 +3,19 @@ class Ripper(object):
 		if mp3(m_file):
 			self.editable = mp3(m_file)
 			self.main()
-			#except ID3NoHeaderError:
-			print "File does not exist, or is not an mp3 file."
 	def main(self):
 		#editable   = mp3(self.mp3_file)
 		f_artist   = self.editable['artist'][0].encode('utf-8')
 		f_title    = self.editable['title'][0].encode('utf-8')
 		f_cd_num   = self.editable['discnumber'][0].encode('utf-8')
 		f_song_num = self.editable['tracknumber'][0].encode('utf-8')
+		f_album    = self.editable['album'][0].encode('utf-8')
 		#print editable
 		if f_cd_num:
 			f_cd_num=f_cd_num[0]
 		if f_song_num:
 			f_song_num=f_song_num[0]
-		song_data = {'title':f_title,'artist':f_artist,'cd_number':f_cd_num,'track_number':f_song_num}
+		song_data = {'title':f_title,'artist':f_artist,'cdnumber':f_cd_num,'tracknumber':f_song_num, 'album':f_album}
 		print song_data
 		self.song_data = song_data
 
@@ -26,12 +25,15 @@ if __name__ == "__main__":
 	from libs.mut.mutagen.easyid3 import EasyID3 as mp3
 	#import parse_config TODO
 	base_args = argv
-	modified = base_args[1]
+	try: 
+		modified = base_args[1]
+	except:
+		raise Exception('\033[1;38mNo target file specified\033[1;m') #formatt error to grab attention
 	script = base_args[0]
 	try:
 		dat = Ripper(modified)
 	except:
-		print "File does not exist, is corrupted, or is not an mp3"
+		raise Exception("File does not exist, is not an mp3, or is corrupted.")
 	else:
 		print dat
 		print dat.song_data
