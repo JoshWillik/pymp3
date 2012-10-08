87453f988327f5a1e683373fffcebcb297dbe791
Josh Vandewillik <joshwillik@gmail.com>
2012-10-08 11:36:50 -0400

    add target error checking and target file option

--- modules/name_paste.py
+++ modules/name_paste.py
@@ -1,9 +1,10 @@
 class Paster(object):
-	def __init__(self, song_data, parse_array):
+	def __init__(self, song_data, parse_array,target_file):
 		self.song_data = song_data
 		self.parsed_config = parse_array
 		print song_data
 		print parse_array
+		print target_file
 
 
 if __name__ == "__main__":
@@ -17,10 +18,13 @@ if __name__ == "__main__":
 
 	#get filename to pass to file renamer
 	from sys import argv
-	#target = argv[1]
+	try:
+		target = argv[1]
+	except IndexError:
+		raise Exception("no target file specified")
 	script = argv[0]
 
 	#get song data to pass to file renamer
 	import file_rip
 	ripped = file_rip.Ripper('modules/test/foo.mp3').song_data
-	start_paste = Paster(ripped, parse_format)
+	start_paste = Paster(ripped, parse_format, target)
