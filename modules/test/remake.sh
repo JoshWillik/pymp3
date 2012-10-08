05bf1a905080d62d3c40b026b623e32cef5eb460
Josh Vandewillik <joshwillik@gmail.com>
2012-10-08 12:48:57 -0400

    correct test file reset script

--- modules/test/remake.sh
+++ modules/test/remake.sh
@@ -1,9 +1,12 @@
-#! /bin/sh
-for i in *.mp3
+#! /bin/bash
+for i in *
 do
   echo $i
-  if [ ! "$i" = "lar.mp3" ]; then
-    rm $i
+  if [[ ! "$i" == "lar.mp3" && ${i#*.} == "mp3" ]]; then
+    echo ${i#*.}
+    rm "$i"
+    echo $i "deleted"
   fi
 done
 cp lar.mp3 foo.mp3
+sleep 2m
